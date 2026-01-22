3a0e588b8e207ab2901fb5d31fe725e56f63e062
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-01-22 17:08:05 -0500

    Clean-up code + write new comments for pseudocode

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const xBtn = document.getElementById("xRadioButton");
 const oBtn = document.getElementById("oRadioButton");
 const usernameInput = document.getElementById("username");
 
-
 //object to store the gameboard itself as an array
 const Gameboard = (function(){
     const rows = 3;
@@ -20,8 +19,6 @@ const Gameboard = (function(){
         board.push(row);
     }
         
-    console.log(board);
-
     // retrieve current state of gameboard
     function getBoard(){
         return board.map(row => [...row]);
@@ -46,6 +43,7 @@ const Gameboard = (function(){
 
 })();
 
+//Factory to render the board to the DOM
 function RenderBoard(){
 
 };
@@ -53,35 +51,24 @@ function RenderBoard(){
 // Factory to control the flow/logic of the game
 function Squares(){
     //default value of any square on the board will be 0 until changed.
-    let value = 0;
+    let squareValue = 0;
 
     //function to take player choice as param and change value of cell to the value of the player
     const changeValue = (playerValue) => {
-        value = playerValue;
+        squareValue = playerValue;
     }
 
-    
+    //retrieve current value of square
     const getValue = () => value;
-
 
     return { changeValue, getValue };
 }
-
-
-
-// Don't worry about user inputs for now; just call game logic functions with whichever argument you need
-
-
-
-// Object that will create the DOM for the gameboard
-// Store a function in this object that renders the gameboard to the webpage
-//  
 
 function GameController(playerOne = usernameInput, playerTwo = "Computer"){
     //to take username input from html and store as playerOne name
     // const username = document.getElementById("username");
 
-    // default value of square will be 0. When clicked by playerOne, it will be 1. When clicked by playerTwo, it will be 2.
+    //create player objects and assign them a value
     const players = [
         {name: playerOne, value: 1},
         {name: playerTwo, value: 2}
@@ -102,8 +89,6 @@ function GameController(playerOne = usernameInput, playerTwo = "Computer"){
     //function to retrieve who's turn it currently is
     const getCurrentPlayer = () => currentPlayer;
 
-
-
     const displayTurn = () => {
         console.log(`It's ${getCurrentPlayer().name}'s turn!`)
         // We will display via DOM manipulation who's turn it is later
@@ -117,9 +102,15 @@ function GameController(playerOne = usernameInput, playerTwo = "Computer"){
     };
     
     const checkWinner = () => {
-        // check who winner is
-        // if there IS a winner, display modal congratulating winner; then reset the board.
-        // if there is no winner, then switch who's turn it is and return the updated board.
+        if(index){
+            //Check if three squares in a row have the same value.
+            //display winner msg + end game.
+            //reset board.
+        } else if (notindex){
+            // alternative win con
+        } else {
+            // don't end game and simply switch turns.
+        }
     }
 
     return { }
@@ -136,13 +127,7 @@ function GameController(playerOne = usernameInput, playerTwo = "Computer"){
 
 // Assign proper selection to user and the opposite to the computer
 
-// The game begins, all gameboard squares/inputs need to be emptied.
 
-// When clicked, each button needs to check whether that square is already populated
-// if already populated, then return an error and end execution
-// if not already populated, store value of who clicked it (user OR computer)
-
-// Need logic for how the game functions. Check if three squares in a row are checked.
 // End the game when user/computer get's 3 in a row, OR, when there are no more available spaces on the board (A TIE GAME)
 // Return a result of who won, display a reset/play again button
 // Clear the board and allow the user to re-pick whether they are X's and O's
